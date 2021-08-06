f3dbc8bcad6a814a30308d56157883d524b0339f
j100667 <jeanzenzeluk@hotmail.com>
2021-08-06 14:02:46 -0300

    Teste de Commit

--- src/main/java/io/github/dougllasfps/config/SecurityConfig.java
+++ src/main/java/io/github/dougllasfps/config/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
     @Autowired
     private UsuarioServiceImpl usuarioService;
     @Autowired
