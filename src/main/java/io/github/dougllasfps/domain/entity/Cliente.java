c7398dcfe206cbb2741c37e4f6caad32e79f0ebf
j100667 <jeanzenzeluk@hotmail.com>
2021-08-11 17:16:42 -0300

    Finalizado CRUD de Clientes e Produtos

--- src/main/java/io/github/dougllasfps/domain/entity/Cliente.java
+++ src/main/java/io/github/dougllasfps/domain/entity/Cliente.java
@@ -17,6 +17,11 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
+
+
     @JsonIgnore
     @OneToMany( mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
@@ -55,6 +60,14 @@ public class Cliente {
 
     public void setPedidos(Set<Pedido> pedidos) {
         this.pedidos = pedidos;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     @Override
