7792128f472a38d9f33817cb428771983c8ec4a6
j100667 <jeanzenzeluk@hotmail.com>
2021-08-21 19:04:43 -0300

    Add Lombok, springdevtools, e pedido Controller.

---
+++ src/main/java/io/github/dougllasfps/rest/ApiErros.java
@@ -0,0 +1,17 @@
+package io.github.dougllasfps.rest;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErros {
+
+    @Getter
+    private List<String> erros;
+
+    public ApiErros(String mensagemErro){
+        this.erros = Arrays.asList(mensagemErro);
+    }
+}
--- src/main/java/io/github/dougllasfps/service/PedidoService.java
+++ src/main/java/io/github/dougllasfps/service/PedidoService.java
@@ -1,4 +1,14 @@
 package io.github.dougllasfps.service;
 
+import io.github.dougllasfps.domain.entity.Pedido;
+import io.github.dougllasfps.rest.dto.PedidoDTO;
+
+import java.util.Optional;
+
 public interface PedidoService {
+    Pedido salvar (PedidoDTO dto);
+
+    Optional<Pedido> obterPedidoCompleto(Integer idPedido);
 }
+
+
